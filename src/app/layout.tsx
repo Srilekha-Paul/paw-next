5fbc0a8cd2291d1aa055f0e76779cb9afa947797
Srilekha Paul <srilekhapaul2003@gmail.com>
2026-02-26 21:30:32 +0530

    layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,82 +1,7 @@
-// import "./globals.css"
-// import { ThemeProvider } from "next-themes"
-// import Navbar from "@/components/navbar"
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body className="min-h-screen bg-background font-sans antialiased">
-//         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-//           <Navbar />
-//           {children}
-//         </ThemeProvider>
-//       </body>
-//     </html>
-//   )
-// }
-
-
-// import "./globals.css"
-// import type { Metadata } from "next"
-
-// export const metadata: Metadata = {
-//   title: "PawBite",
-//   description: "Healthy & Natural Dog Food",
-// }
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className="bg-[#f4ece4] min-h-screen">
-//         {children}
-//       </body>
-//     </html>
-//   )
-// }
-
-
-
-
-// "use client"
-// import { CartProvider } from "@/context/CartContext"
-// import "./globals.css"
-// import { ThemeProvider } from "next-themes"
-// import Navbar from "@/components/navbar"
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body>
-//         <ThemeProvider attribute="class" defaultTheme="light">
-//           <Navbar />
-//           {children}
-//         </ThemeProvider>
-
-//           <CartProvider>
-//           {children}
-//        </CartProvider>
-//       </body>
-//     </html>
-//   )
-// }
-
-
-
 import "./globals.css"
 import Navbar from "@/components/navbar"
 import { CartProvider } from "@/context/CartContext"
+import { ThemeProvider } from "next-themes"
 
 export const metadata = {
   title: "PawBite",
@@ -89,12 +14,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <CartProvider>
-          <Navbar />
-          {children}
-        </CartProvider>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <CartProvider>
+            <Navbar />
+            {children}
+          </CartProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
